6ce87631a3ad59643de6536c6b33601721fcb36c
Gee <glynester@hotmail.com>
2022-02-28 20:36:21 +0000

    Added 3 more props

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import CommentDetail from './CommentDetail';
 const App=()=>{
   return (
     <div className = "ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Jane" />
+      <CommentDetail author="Sam" timeAgo="Today at 6:00PM" content="Nice post!" avatar={faker.image.image()} />
+      <CommentDetail author="Alex" timeAgo="Today at 6:25PM" content="Cool post!" avatar={faker.image.image()} />
+      <CommentDetail author="Jane" timeAgo="Today at 7:06PM" content="Awesome post!" avatar={faker.image.image()} />
     </div>
   )
 }
