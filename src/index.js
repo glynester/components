c49e879c2dfd8cfb68586c685a9a93c3ec67b98e
Gee <glynester@hotmail.com>
2022-03-01 09:23:57 +0000

    Set up child component and reused component

--- src/index.js
+++ src/index.js
@@ -2,13 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App=()=>{
   return (
     <div className = "ui container comments">
-      <CommentDetail author="Sam" timeAgo="Today at 6:00PM" content="Nice post!" avatar={faker.image.image()} />
-      <CommentDetail author="Alex" timeAgo="Today at 6:25PM" content="Cool post!" avatar={faker.image.image()} />
-      <CommentDetail author="Jane" timeAgo="Today at 7:06PM" content="Awesome post!" avatar={faker.image.image()} />
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail author="Sam" timeAgo="Today at 6:00PM" content="Nice post!" avatar={faker.image.image()} />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail author="Alex" timeAgo="Today at 6:25PM" content="Cool post!" avatar={faker.image.image()} />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail author="Jane" timeAgo="Today at 7:06PM" content="Awesome post!" avatar={faker.image.image()} />
+      </ApprovalCard>
     </div>
   )
 }
